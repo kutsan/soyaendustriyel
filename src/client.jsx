81a28fd3d5604fd6496b356a879d1c93aee5e8de
Kutsan Kaplan <me@kutsankaplan.com>
2020-02-18 20:28:14 +0300

    Add react-router

--- src/client.jsx
+++ src/client.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 
 function render(Component) {
-	ReactDOM.render(<Component />, document.getElementById('root'))
+	ReactDOM.render(
+		<BrowserRouter>
+			<Component />
+		</BrowserRouter>,
+		document.getElementById('root')
+	)
 }
 
 render(App)
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+import { Link } from 'react-router-dom'
+
+export default class Footer extends Component {
+	render() {
+		return <footer>2020, Reyhan Unal & Kutsan Kaplan</footer>
+	}
+}
