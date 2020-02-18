ace383bc0547ac8642e6d8d7160a29e77c28a71a
Kutsan Kaplan <me@kutsan.dev>
2020-02-18 20:28:14 +0300

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 
+// Screens
+import Home from './screens/Home'
+import About from './screens/About'
+
+// Components
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+
+// Styles
 import './style.css'
 
-const App = () => <>Initial render.</>
+const App = () => {
+	return (
+		<>
+			<Header />
+
+			<main>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/about' component={About} />
+				</Switch>
+			</main>
+
+			<Footer />
+		</>
+	)
+}
 
 export default App
