cd89c8728503d0eabd5f4b8e394673c4fed0790e
Kutsan Kaplan <me@kutsan.dev>
2020-03-17 21:21:44 +0300

    Add major brands with their logos to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import Brands from './components/Brands.jsx'
+
 import './Footer.css'
 
 const Footer = () => {
-	return <footer>done with love by reyhan & kutsan in 2020</footer>
+	return (
+		<footer>
+			<Brands />
+
+			<span>done with love by reyhan & kutsan in 2020</span>
+		</footer>
+	)
 }
 
 export default Footer
