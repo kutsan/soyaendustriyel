c74963a3ecb28d8967f22d26c572e38ea84b50e7
Kutsan Kaplan <me@kutsankaplan.com>
2020-03-22 20:06:53 +0300

    Add Products route

---
+++ src/routes/Products/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Products extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return <>Hey from Products</>
+	}
+}
