afa228f8838dd8c721fdbd1bf417683361c02bbb
Kutsan Kaplan <me@kutsan.dev>
2020-03-22 20:06:53 +0300

    Add Products route

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { Route, Switch } from 'react-router-dom'
 // Routes
 import Home from './routes/Home'
 import About from './routes/About'
+import Products from './routes/Products'
 
 // Components
 import Header from './components/Header/Header.jsx'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import NotFound from './components/NotFound/NotFound.jsx'
 
 // Styles
 import './App.css'
@@ -24,6 +26,8 @@ const App = () => {
 				<Switch>
 					<Route exact path='/' component={Home} />
 					<Route path='/about' component={About} />
+					<Route path='/products/:category' component={Products} />
+					<Route component={NotFound} />
 				</Switch>
 			</main>
 
