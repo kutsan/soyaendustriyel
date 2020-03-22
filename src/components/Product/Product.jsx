6906ca345a32cd72738376d6dab00a017f608fa1
Kutsan Kaplan <me@kutsan.dev>
2020-03-22 21:56:48 +0300

    Format code

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 import './Product.css'
 
 const Product = () => {
-	return (
-		<div className='product'>Product</div>
-	)
+	return <div className='product'>Product</div>
 }
 
 export default Product
