0614f1a6ad4473ecc4678a7eec4c9ede03fd08e7
Kutsan Kaplan <me@kutsankaplan.com>
2020-03-30 07:59:49 +0300

    Make NavbarWeb seperate component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-import NavbarMobile from './NavbarMobile.jsx'
-
-// Styles
-import './Navbar.css'
-
-// Data
-import { categories } from '../../data/categories.json'
-
-const getSubCategories = (category) => {
-	return categories.filter((x) => x.parent === category.id)
-}
-
-const mainCategories = categories.filter((x) => !x.parent)
+import NavbarMobile from './components/NavbarMobile.jsx'
+import NavbarWeb from './components/NavbarWeb.jsx'
 
 const Navbar = () => {
 	return (
 		<nav>
 			<NavbarMobile />
-			<div className='navbar-menu'>
-				{mainCategories.map((x) => (
-					<div key={x.id} className='navbar-menu--item'>
-						<Link to={`/products/${x.id}`}>{x.name}</Link>
-						<div className='navbar-menu--submenu'>
-							{getSubCategories(x).map((y) => (
-								<div key={y.id} className='navbar-menu--item'>
-									<Link to={`/products/${x.id}/${y.id}`}>{y.name}</Link>
-								</div>
-							))}
-						</div>
-					</div>
-				))}
-			</div>
+			<NavbarWeb />
 		</nav>
 	)
 }
