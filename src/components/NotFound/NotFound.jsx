d756c24a5e02a8ad29fb834cfad18c0b5ba91c39
Kutsan Kaplan <me@kutsan.dev>
2020-03-30 07:18:18 +0300

    Improve NotFound page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './NotFound.css'
+
+import NotFoundIcon from './NotFound.svg'
 
 const NotFound = () => {
-	return <div>Not found. Go to the homepage.</div>
+	return (
+		<div className='notfound'>
+			<i className='notfound__icon' dangerouslySetInnerHTML={{ __html: NotFoundIcon }} />
+			<div className='notfound__info'>Aradığınız sayfayı bulamadık.</div>
+			<div className='notfound__more-info'>
+				Sayfa kaldırılmış veya değiştirilmiş olabilir.
+			</div>
+
+			<Link to='/'>
+				<button className='notfound__go-home-page'>ANA SAYFAYA GİT</button>
+			</Link>
+		</div>
+	)
 }
 
 export default NotFound
