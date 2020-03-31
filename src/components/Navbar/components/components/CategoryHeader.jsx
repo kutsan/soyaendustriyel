f55d0eeefe7a5ffa15ff76ab5d7fe8217722ea67
Kutsan Kaplan <me@kutsankaplan.com>
2020-04-01 00:53:09 +0300

    Refactor NavbarMobile

---
+++ src/components/Navbar/components/components/CategoryHeader.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import './CategoryHeader.css'
+
+const CategoryHeader = ({
+	modifier,
+	item,
+	to,
+	onClickLink,
+	onClickToggle,
+	expanded,
+	hasCategoriesUnder
+}) => {
+	return (
+		<div
+			className={`navbar-mobile__category-header navbar-mobile__category-header--${modifier}`}
+		>
+			<Link
+				to={to}
+				onClick={onClickLink}
+				className={`navbar-mobile__category-link navbar-mobile__category-link--${modifier} ${
+					expanded ? 'navbar-mobile__category-link--expanded' : ''
+				}`}
+			>
+				{item.name}
+			</Link>
+
+			{hasCategoriesUnder && (
+				<button
+					onClick={onClickToggle}
+					key={item.id}
+					className={`navbar-mobile__category-toggle navbar-mobile__category-toggle--${modifier} ${
+						expanded ? 'navbar-mobile__category-toggle--expanded' : ''
+					}`}
+				>
+					<span />
+				</button>
+			)}
+		</div>
+	)
+}
+
+CategoryHeader.propTypes = {
+	modifier: PropTypes.string.isRequired,
+	hasCategoriesUnder: PropTypes.bool.isRequired,
+	item: PropTypes.object.isRequired,
+	to: PropTypes.string.isRequired,
+	onClickLink: PropTypes.func.isRequired,
+	expanded: PropTypes.bool,
+	onClickToggle: PropTypes.func
+}
+
+export default CategoryHeader
