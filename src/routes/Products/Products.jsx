52eeec306be5e76717ee6572955986284fed1519
Kutsan Kaplan <me@kutsankaplan.com>
2020-04-01 00:53:21 +0300

    Convert Products to function component

--- src/routes/Products/Products.jsx
+++ src/routes/Products/Products.jsx
@@ -10,26 +10,22 @@ import './Products.css'
 // Data
 import { products } from './../../data/products.json'
 
-export default class Products extends React.Component {
-	constructor(props) {
-		super(props)
-	}
+const Products = ({ match }) => {
+	return (
+		<>
+			<div className='products-header'>{match.url}</div>
 
-	render() {
-		return (
-			<>
-				<div className='products-header'>{this.props.match.url}</div>
-
-				<div className='products-container'>
-					{products.map((cur) => {
-						return <ProductCard key={cur.id} product={cur} />
-					})}
-				</div>
-			</>
-		)
-	}
+			<div className='products-container'>
+				{products.map((cur) => {
+					return <ProductCard key={cur.id} product={cur} />
+				})}
+			</div>
+		</>
+	)
 }
 
 Products.propTypes = {
 	match: PropTypes.object
 }
+
+export default Products
