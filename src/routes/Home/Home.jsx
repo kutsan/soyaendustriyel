35587e2bb340e7caf99df54b8e3c87d22e16f229
Kutsan Kaplan <me@kutsan.dev>
2020-04-08 19:16:39 +0300

    Add temporary items to Home route

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
+import ProductCard from '@/components/ProductCard/ProductCard.jsx'
 import Brands from '@/components/Brands/Brands.jsx'
 
+import { products } from '@/data/products.json'
+
 const Home = () => {
+	function shuffle(a) {
+		for (let i = a.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1))
+			;[a[i], a[j]] = [a[j], a[i]]
+		}
+
+		return a
+	}
+
 	return (
 		<>
-			Home page here.
+			<div className='products'>
+				{shuffle(products)
+					.slice(0, 8)
+					.map((cur) => {
+						return <ProductCard key={cur.id} product={cur} />
+					})}
+			</div>
+
 			<Brands />
 		</>
 	)
