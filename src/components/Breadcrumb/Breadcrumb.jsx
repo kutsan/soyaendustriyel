b4da0613a03a760b5405c9ea90ba35becae84a11
Kutsan Kaplan <me@kutsankaplan.com>
2020-04-08 12:59:23 +0300

    Add UI to Breadcrumb component

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 import './Breadcrumb.css'
 
-const Breadcrumb = () => {
-	return <div className='breadcrumb'>breadcrumb</div>
+const Breadcrumb = ({ steps }) => {
+	return (
+		<div className='breadcrumb'>
+			{steps.map((e, index) => (
+				<div key={index} className='breadcrumb__step'>
+					{e.to ? (
+						<Link className='breadcrumb__step-link' to={e.to}>
+							{e.name}
+						</Link>
+					) : (
+						<span>{e.name}</span>
+					)}
+				</div>
+			))}
+		</div>
+	)
+}
+
+Breadcrumb.propTypes = {
+	steps: PropTypes.array.isRequired
 }
 
 export default Breadcrumb
