22643d0b25af53c922355c7244065f5d5045a684
Reyhan Unal <rreyhanunal@gmail.com>
2020-04-10 00:48:31 +0300

    Add Image component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,48 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-
-import storage from '@/utils/firebase/storage.js'
+import Image from '@/components/Image/Image.jsx'
 
 import './ProductCard.css'
 
 const ProductCard = ({ product }) => {
-	const [img, setImg] = useState(null)
-	const [loading, setLoading] = useState(false)
-
-	useEffect(() => {
-		let ignore = false
-
-		setLoading(true)
-
-		storage
-			.child(`${product.id}.jpg`)
-			.getDownloadURL()
-			.then((url) => {
-				if (!ignore) {
-					setLoading(false)
-					setImg(url)
-				}
-			})
-			.catch((err) => {
-				if (!ignore) console.error(err)
-			})
-
-		return () => {
-			ignore = true
-		}
-	}, [product.id])
-
 	return (
 		<Link className='product-card' to={`/product/${product.id}`}>
-			<div className='product-card__picture-container'>
-				<div
-					style={img && { backgroundImage: `url(${img})` }}
-					className={`product-card__picture ${
-						loading ? 'product-card__picture--loading' : ''
-					} ${!loading && !img ? 'product-card__picture--no-image' : ''}`}
-				></div>
-			</div>
+			<Image name={`${product.id}.jpg`} />
 			<div className='product-card__code'>{product.code}</div>
 			<div className='product-card__name'>{product.name}</div>
 		</Link>
