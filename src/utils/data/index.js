4cf924897b819bfcdd9e7e2431be2a0fe6ba9439
Kutsan Kaplan <me@kutsankaplan.com>
2020-04-10 18:47:17 +0300

    Add data API utils

---
+++ src/utils/data/index.js
@@ -0,0 +1,4 @@
+import product from './product.js'
+import category from './category.js'
+
+export default { product, category }
---
+++ src/utils/data/product.js
@@ -0,0 +1,49 @@
+import { product as dataProduct } from '@/data/product.json'
+
+/**
+ * Gets product reference with ID.
+ *
+ * @param {string} id - Product ID
+ * @return {Object} Direct product reference
+ **/
+const getRef = (id) => {
+	return dataProduct.find((e) => e.id === id)
+}
+
+/**
+ * Gets random products from optionally given category.
+ *
+ * @param {number} count - Product count
+ * @param [string] category - Optional category to be used to filter
+ * @return {Object[]} Array of random products
+ **/
+const getRandom = (count, category = null) => {
+	const shuffle = (arr) => {
+		const a = [...arr]
+
+		if (count > arr.length) {
+			count = arr.length
+		}
+
+		for (let i = count - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1))
+
+			;[a[i], a[j]] = [a[j], a[i]]
+		}
+
+		return a
+	}
+
+	return category
+		? shuffle(dataProduct.filter((e) => e.category === category))
+		: shuffle(dataProduct)
+}
+
+/**
+ * Filter product with Array.prototype.filter()
+ **/
+const filter = (cb) => {
+	return dataProduct.filter(cb)
+}
+
+export default { getRef, getRandom, filter }
