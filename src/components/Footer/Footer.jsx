384b8cb278085c23a3f669dbc1de5ee995c647cd
Kutsan Kaplan <me@kutsankaplan.com>
2020-04-21 13:07:13 +0300

    Add Attribution component to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import './Footer.css'
 import About from './components/About.jsx'
 import Contact from './components/Contact.jsx'
 import Copyright from './components/Copyright.jsx'
+import Attribution from './components/Attribution.jsx'
 
 const Footer = () => {
 	return (
@@ -15,6 +16,7 @@ const Footer = () => {
 			</div>
 
 			<Copyright />
+			<Attribution />
 		</footer>
 	)
 }
