9b07d63ccb1ed328499b95239a1778ccea7a1f87
Kutsan Kaplan <me@kutsankaplan.com>
2020-04-22 20:04:39 +0300

    Comment AppContext.js for now

---
+++ src/context/AppContext.js
@@ -0,0 +1,14 @@
+// import React, { useState } from 'react'
+// import PropTypes from 'prop-types'
+
+// const AppContext = React.createContext(null)
+
+// const AppProvider = ({ children }) => {
+// 	return <AppContext.Provider value={null}>{children}</AppContext.Provider>
+// }
+
+// AppProvider.propTypes = {
+// 	children: PropTypes.node
+// }
+
+// export { AppProvider, AppContext }
