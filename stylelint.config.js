57bbf0016209d219e6deea11caa1458592c2bd95
Kutsan Kaplan <me@kutsan.dev>
2020-04-26 03:07:19 +0300

    Use @kutsan/config for stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,14 +1,1 @@
-module.exports = {
-	extends: ['stylelint-config-standard', 'stylelint-order-config-yandex'],
-	plugins: ['stylelint-order'],
-	rules: {
-		indentation: 'tab',
-		'declaration-empty-line-before': [
-			'never',
-			{
-				ignore: ['after-comment', 'after-declaration', 'inside-single-line-block']
-			}
-		],
-		'color-hex-length': 'long'
-	}
-}
+module.exports = require('@kutsan/config/stylelint')
