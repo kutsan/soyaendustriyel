7171b80e7eacfd20046bf2dfcf8232e798a0c36b
Kutsan Kaplan <me@kutsan.dev>
2020-04-30 00:12:41 +0300

    Remove unused height of useViewport()

--- src/hooks/use-viewport.js
+++ src/hooks/use-viewport.js
@@ -3,9 +3,9 @@ import { useContext } from 'react'
 import { ViewportContext } from '@/context/ViewportContext.js'
 
 const useViewport = () => {
-	const { width, height } = useContext(ViewportContext)
+	const { width } = useContext(ViewportContext)
 
-	return { width, height }
+	return { width }
 }
 
 export default useViewport
