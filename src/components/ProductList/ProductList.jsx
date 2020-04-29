abd31005145c1177777e7d0b360f742df7d9add7
Kutsan Kaplan <me@kutsan.dev>
2020-04-30 01:13:33 +0300

    Remove unused prop className from ProductList

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,9 +5,9 @@ import './ProductList.css'
 
 import ProductCard from '@/components/ProductCard/ProductCard'
 
-const ProductList = ({ items, className }) => {
+const ProductList = ({ items }) => {
 	return (
-		<div className={`product-list ${className ? className : ''}`}>
+		<div className='product-list'>
 			{items.map((item) => (
 				<div key={item.id} className='product-list__item'>
 					<ProductCard product={item} />
@@ -18,8 +18,7 @@ const ProductList = ({ items, className }) => {
 }
 
 ProductList.propTypes = {
-	items: PropTypes.array,
-	className: PropTypes.string
+	items: PropTypes.array
 }
 
 export default ProductList
