15cd9974f366a04ae6332d4f818f7a5b09e49063
Kutsan Kaplan <me@kutsan.dev>
2021-01-18 02:53:48 +0300

    add initial TypeScript support and remove Babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,28 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb-base', 'prettier', 'prettier/@typescript-eslint'],
   env: {
     node: true,
     browser: true,
     jest: true,
     es6: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 11,
-    sourceType: 'module'
-  },
   settings: {
     react: { version: 'detect' }
   },
   rules: {
     semi: 'off',
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['off', 'single'],
-    'no-console': 'off',
-    'react/prop-types': 'warn'
-  }
+    'comma-dangle': 'off'
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended'
+      ]
+    }
+  ]
 }
